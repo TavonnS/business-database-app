cd75b127f664c76c8113958355754ce48dbe0c9b
TavonnS <142205891+TavonnS@users.noreply.github.com>
2023-11-15 21:10:22 -0800

    seeding

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ USE business_db;
 
 CREATE TABLE department (
     id INT PRIMARY KEY,
-    name VARCHAR(30),
-    
+    name VARCHAR(30)
+
 );
 
 CREATE TABLE role (
@@ -23,7 +23,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id)
+    FOREIGN KEY (role_id) REFERENCES role(id),
     manager_id INT, 
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,18 @@
 INSERT INTO department (id, name)
-VALUES  (1, Sales),
-        (2, Legal),
-        (3, Finance),
-        (4, Engineering);
+VALUES  (1, 'Sales'),
+        (2, 'Legal'),
+        (3, 'Finance'),
+        (4, 'Engineering');
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES  (1, 'Sales Lead', 100000, 'Sales'),
-        (2, 'Salesperson', 80000, 'Sales'),
-        (3, 'Lead Engineer', 10000, 'Engineering'),
-        (4, 'Software Engineer', 10000, 'Engineering'),
-        (5, 'Account Manager', 1, 'Finance'),
-        (6, 'Accountant', 1, 'Finance'),
-        (7, 'Legal Team Lead', 1, 'Legal'),
-        (8, 'Lawyer', 1, 'Legal');
+VALUES  (1, 'Sales Lead', 100000.00, 1),
+        (2, 'Salesperson', 100000.00, 1),
+        (3, 'Lead Engineer', 100000.00, 4),
+        (4, 'Software Engineer', 100000.00, 4),
+        (5, 'Account Manager', 100000.00, 3),
+        (6, 'Accountant', 100000.00, 3),
+        (7, 'Legal Team Lead', 100000.00, 2),
+        (8, 'Lawyer', 100000.00, 2);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES  (1, 'Johnny', 'Carson', 1, null),
