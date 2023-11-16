186c8819312ebb6226bda60891910ae49b242798
TavonnS <142205891+TavonnS@users.noreply.github.com>
2023-11-16 11:53:44 -0800

    mysql2

--- mysql2.js
+++ mysql2.js
@@ -1,11 +1,16 @@
 // get the client
 const mysql2 = require('mysql2');
 
+// import env variables
+require('dotenv').config();
+const password = process.env.PASSWORD;
+
 // create the connection to database
 const connection = mysql2.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'business_db'
+  database: 'business_db',
+  password: password
 });
 
 
